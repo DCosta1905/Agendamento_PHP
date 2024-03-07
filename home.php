4f81285ec2b82a71a23b9fad3f00d1da009edf96
DaviCostaIf <davicosta9204@gmail.com>
2024-03-07 20:06:39 -0300

    Update home

--- home.php
+++ home.php
@@ -23,17 +23,19 @@ if (!isset($_SESSION['loggedin'])) {
 <body class="loggedin">
   <nav class="navtop">
     <div>
-      <h1>Website Title</h1>
-      <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+      <h1>Site - Fisioterapeuta Monica Oliveira</h1>
+      <a href="pacients.php"><i class="fas fa-user-circle"></i>Pacientes</a>
       <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
     </div>
   </nav>
-  <div class="content">
-    <h2>Home Page</h2>
-    <p>Welcome back,
-      <?= htmlspecialchars($_SESSION['name'], ENT_QUOTES) ?>!
-    </p>
-  </div>
+  <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF-RK7jK_M&#x2F;l9x1P4t-2CgoFRvmyw_zFw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+
 </body>
 
 </html>
